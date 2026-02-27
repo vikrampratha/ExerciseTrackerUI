e64fa05ca740a07217e8cc2cd45fda8e8d589168
vikrampratha <vikram.pratha@gmail.com>
2026-02-27 04:03:16 -0500

    Added circle button to about tab

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,9 +1,17 @@
+import CircleButton from '@/components/CircleButton';
 import { StyleSheet, Text, View } from 'react-native';
+
+const onAdd = () => {
+  //TODO
+};
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>About screen</Text>
+      <View style={styles.fab}>
+        <CircleButton onPress={onAdd} />
+      </View>
     </View>
   );
 }
@@ -14,6 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: 50,
   },
   text: {
     color: '#fff',

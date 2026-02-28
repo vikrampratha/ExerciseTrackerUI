a5d92d1b6fa0a22bd92c56c533e4df246fdbb731
vikrampratha <vikram.pratha@gmail.com>
2026-02-28 18:48:38 -0500

    Added calendar view of workouts to Home Screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Workout, getWorkouts } from "@/services/api";
+import { useEffect, useMemo, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Calendar } from 'react-native-calendars';
+
 
 export default function Index() {
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      try {
+        const data = await getWorkouts();
+        setWorkouts(data);
+      } catch (error) {
+        console.error('Failed to fetch workouts:', error);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
+  const markedDates = useMemo(() => {
+    const marks: Record<string, any> = {};
+
+    workouts.forEach(workout => {
+      marks[workout.date] = {
+        customStyles: {
+          container: {
+            borderWidth: 1.5,
+            borderColor: '#2ECC71',
+            borderRadius: 20,
+            justifyContent: 'center'
+          },
+          text: {
+            color: '#fff',
+          },
+        },
+      };
+    });
+
+    return marks;
+  }, [workouts]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Calendar
+        current={new Date().toISOString().split('T')[0]}
+        maxDate={new Date().toISOString().split('T')[0]}
+        markingType="custom"
+        markedDates={markedDates}
+        theme={{
+          calendarBackground: '#25292e',
+          dayTextColor: '#fff',
+          monthTextColor: '#fff',
+          arrowColor: '#fff',
+        }}
+      />
     </View>
   );
 }
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+export interface Workout {
+  id: number;
+  date: string;
+  type: string;
+  //exercises: Exercise[];
+};
+
 export const api = axios.create({
-  //baseURL: 'http://localhost:8080',
   baseURL: 'http://192.168.1.151:8080',
   headers: {
     'Content-Type': 'application/json',
@@ -13,3 +19,8 @@ export const postWorkout = async (payload: any) => {
   const response = await api.post('/createWorkout', payload);
   return response.data;
 };
+
+export const getWorkouts = async () => {
+    const response = await api.get<Workout[]>('getAllWorkouts');
+    return response.data;
+}
